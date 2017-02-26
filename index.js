c3f4f1fa7b354a2615846e2a8b39596d8ad90a7c
Tyman Sin <sin.tyman@yahoo.com>
2017-02-25 21:10:14 -0700

    Added default Amazon Intents

--- index.js
+++ index.js
@@ -17,5 +17,22 @@ var handlers = {
 	
     'DecideRecipe': function () {
         this.emit(':tell', 'Guys, I have made a LOT of new commits in the git hub. This works, I am so happy right now!');
-    }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
+        this.attributes['repromptSpeech'] = this.t("HELP_REPROMT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'SessionEndedRequest':function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+}
 };
