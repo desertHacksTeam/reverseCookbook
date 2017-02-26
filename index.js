38b8538c5f5d46c29e105698c3c655a53224438e
bull610 <blladi610@yahoo.com>
2017-02-25 20:36:12 -0700

    Added node modules for alexa sdk and added some documentation in the index file

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+//Alexa object courtesy of the new alexa sdk
 var Alexa = require('alexa-sdk');
 
+//main function
 exports.handler = function(event, context, callback){
 
     var alexa = Alexa.handler(event, context);
 
 };
 
+//handles the intents
 var handlers = {
-
     'DecideRecipe': function () {
 		
 		this.emit(':ask', 'Tell me something about yourself', 'Come again, senior?');
         this.emit(':tell', 'You know, I dont really care');
-
     }
-
 };
