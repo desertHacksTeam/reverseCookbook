1f10a58bb4a9461a08234163d6e7c9b0a1c02311
bull610 <blladi610@yahoo.com>
2017-02-25 20:49:18 -0700

    Reworking the index file so that it will hopefully work. I think I forgot a lot of the core components last time, so hopefully this time it works

--- index.js
+++ index.js
@@ -3,25 +3,28 @@ var Alexa = require('alexa-sdk');
 
 //main function
 exports.handler = function(event, context, callback){
-
     var alexa = Alexa.handler(event, context);
-
+	alexa.registerHandlers(handlers);
+	alexa.execute();
 };
 
 //handles the intents
 var handlers = {
+	//instant called on launch
+	'LaunchRequest': function () {
+		his.emit(':tell', 'Tyman, Ive made a LOT of new commits in the git hub');
+		
+		this.emit(':responseReady'); // Called after the response is built but before it is returned to the Alexa service. Calls :saveState.
+		this.emit(':saveState', false); // Handles saving the contents of this.attributes and the current handler state to DynamoDB and then sends the previously built response to the Alexa service. Override if you wish to use a different persistence provider. The second attribute is optional and can be set to ‘true’ to force saving.
+		this.emit(':saveStateError'); // Called if there is an error while saving state. Override to handle any errors yourself.
+	}
+	
     'DecideRecipe': function () {
 		//this.emit(':ask', 'Tell me something about yourself', 'Come again, senior?');
         this.emit(':tell', 'You know, I dont really care');
 		
-		//some random crap
-		var cardTitle = 'Hello World Card';
-
-		var cardContent = 'This text will be displayed in the companion app card.';
-		
-		var imageObj = {
-			smallImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
-			largeImageUrl: 'https://imgs.xkcd.com/comics/standards.png'
-		}
+		this.emit(':responseReady'); // Called after the response is built but before it is returned to the Alexa service. Calls :saveState.
+		this.emit(':saveState', false); // Handles saving the contents of this.attributes and the current handler state to DynamoDB and then sends the previously built response to the Alexa service. Override if you wish to use a different persistence provider. The second attribute is optional and can be set to ‘true’ to force saving.
+		this.emit(':saveStateError'); // Called if there is an error while saving state. Override to handle any errors yourself.
     }
 };
