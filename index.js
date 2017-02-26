a4cf3ef38087cc213600ff98f15dc2366c130afd
bull610 <blladi610@yahoo.com>
2017-02-25 20:51:49 -0700

    fixed a syntax error. There is tno such thing as the 'his' keyword!

--- index.js
+++ index.js
@@ -12,19 +12,11 @@ exports.handler = function(event, context, callback){
 var handlers = {
 	//instant called on launch
 	'LaunchRequest': function () {
-		his.emit(':tell', 'Tyman, Ive made a LOT of new commits in the git hub');
-		
-		this.emit(':responseReady'); // Called after the response is built but before it is returned to the Alexa service. Calls :saveState.
-		this.emit(':saveState', false); // Handles saving the contents of this.attributes and the current handler state to DynamoDB and then sends the previously built response to the Alexa service. Override if you wish to use a different persistence provider. The second attribute is optional and can be set to ‘true’ to force saving.
-		this.emit(':saveStateError'); // Called if there is an error while saving state. Override to handle any errors yourself.
+		this.emit(':tell', 'Tyman, Ive made a LOT of new commits in the git hub');
 	}
 	
     'DecideRecipe': function () {
 		//this.emit(':ask', 'Tell me something about yourself', 'Come again, senior?');
         this.emit(':tell', 'You know, I dont really care');
-		
-		this.emit(':responseReady'); // Called after the response is built but before it is returned to the Alexa service. Calls :saveState.
-		this.emit(':saveState', false); // Handles saving the contents of this.attributes and the current handler state to DynamoDB and then sends the previously built response to the Alexa service. Override if you wish to use a different persistence provider. The second attribute is optional and can be set to ‘true’ to force saving.
-		this.emit(':saveStateError'); // Called if there is an error while saving state. Override to handle any errors yourself.
     }
 };
